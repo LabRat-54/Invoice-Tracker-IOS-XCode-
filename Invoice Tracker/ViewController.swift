6d3d054670c58ed914cf77a8caa2eac973ea4519
Abraham Rubio <a.rubio1224@gmail.com>
2020-08-21 15:14:05 -0400

    Initial Commit

---
+++ Invoice Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Invoice Tracker
+//
+//  Created by Abraham Rubio on 8/21/20.
+//  Copyright © 2020 Abraham Rubio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
