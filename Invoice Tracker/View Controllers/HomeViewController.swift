02cb31e421831a5a98d78663e896ee2e0dde3434
Abraham Rubio <a.rubio1224@gmail.com>
2020-08-22 22:03:35 -0400

    Created UI Prototype

---
+++ Invoice Tracker/View Controllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  Invoice Tracker
+//
+//  Created by Abraham Rubio on 8/22/20.
+//  Copyright © 2020 Abraham Rubio. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
